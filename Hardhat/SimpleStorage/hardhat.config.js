d2fe585b64ff043f50264320e3cc3bb62258b1d8
Dexter <dexterdevmode@gmail.com>
2022-10-16 15:19:02 +0530

    Coverage - a new plugin

--- Hardhat/SimpleStorage/hardhat.config.js
+++ Hardhat/SimpleStorage/hardhat.config.js
@@ -1,5 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox')
 require('./tasks/block-number')
+require('solidity-coverage')
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.17',
